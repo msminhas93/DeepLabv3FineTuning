7ce668abc99001427a444dac665c43b26ad6a33a
Manpreet Singh <msminhas93@gmail.com>
2020-08-08 15:52:45 -0400

    Corrected comment

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from torchvision import models
 def createDeepLabv3(outputchannels=1):
     model = models.segmentation.deeplabv3_resnet101(
         pretrained=True, progress=True)
-    # Added a Sigmoid activation after the last convolution layer
+    # Added a Tanh activation after the last convolution layer
     model.classifier = DeepLabHead(2048, outputchannels)
     # Set the model in training mode
     model.train()
