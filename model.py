29c534bef02f88e591af62fc99dfb3c4ca75e2a0
Manpreet Singh Minhas <msminhas@beluga1.int.ets1.calculquebec.ca>
2020-12-03 21:50:40 -0500

    Created new segmentation dataset
    Updated readme
    
    Added unit tests
    
    Changed cli
    
    Reformatted
    
    Updated requirements.txt for pip
    
    Included envionment.yml for conda

--- model.py
+++ model.py
@@ -1,12 +1,20 @@
 """ DeepLabv3 Model download and change the head for your prediction"""
-from models.segmentation.deeplabv3 import DeepLabHead
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 from torchvision import models
 
 
 def createDeepLabv3(outputchannels=1):
-    model = models.segmentation.deeplabv3_resnet101(
-        pretrained=True, progress=True)
-    # Added a Tanh activation after the last convolution layer
+    """DeepLabv3 class with custom head
+
+    Args:
+        outputchannels (int, optional): The number of output channels
+        in your dataset masks. Defaults to 1.
+
+    Returns:
+        model: Returns the DeepLabv3 model with the ResNet101 backbone.
+    """
+    model = models.segmentation.deeplabv3_resnet101(pretrained=True,
+                                                    progress=True)
     model.classifier = DeepLabHead(2048, outputchannels)
     # Set the model in training mode
     model.train()
